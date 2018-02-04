b1ea63c7740b606ec10ef8cc9b4a32d7ff1a901f
barisbalataci <barisbalataci@gmail.com>
2018-02-04 23:21:36 +0300

    son

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-import config.config_UK_CPI as config
-import eva.data_access.load_data as data
+import json
+import os
+from collections import OrderedDict
+#import eva.data_access.load_data as data
+from eva.data_access.data import data
 
 
 def main():
-    data.load_data(config)
+    config=get_config()
+    csvFile=data.loadfrom_excel(os.getcwd()+"\\data\\UK_DATA_23Jan17.xlsx")
+
+
+
+    #data.load_data()
+
+
+def get_config():
+    config_file = {}
+    with open(os.path.join(os.getcwd() + "\\config", "config.json")) as data_file:
+        config_file = json.load(data_file, object_pairs_hook=OrderedDict)
+    return config_file
 
 
 if __name__ == "__main__":
